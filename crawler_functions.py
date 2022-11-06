944e0f52de5839c09068820841b69a90ee7f7952
MoriningPerson <10195101495@stu.ecnu.edu.cn>
2022-11-06 23:42:40 +0800

    get tropes and indexes from the whole page

--- crawler_functions.py
+++ crawler_functions.py
@@ -12,6 +12,7 @@ from retrying import retry
 BASE_URL = "http://tvtropes.org/pmwiki/pmwiki.php/"
 URL_QUERY = "?action=source"
 
+
 def sanitize_link(link_txt):
     # + shows a specific bullet
     link_txt = link_txt.replace('+ ', '')
@@ -23,6 +24,9 @@ def sanitize_link(link_txt):
     link_txt = link_txt.replace(':', '')
     link_txt = link_txt.replace('\'\'\'', '')
     link_txt = link_txt.replace('\'\'', '')
+    link_txt = link_txt.replace('\\\\', '')
+    link_txt = link_txt.replace('\\', '')
+    link_txt = link_txt.replace('|', '')
     link_txt = link_txt.replace('&quot;', '')
     link_txt = link_txt.replace(',', '')
     link_txt = link_txt.replace('.', '')
@@ -32,6 +36,7 @@ def sanitize_link(link_txt):
     except:
         return ""
     return link_txt
+
 
 @retry(stop_max_attempt_number=10, wait_fixed=2000)
 def download_page_source(title, namespace="Main", delay=0, logging=None):
@@ -55,35 +60,55 @@ def candidates(subindex):
     candidates_index = []
     for i in range(26):
         for j in range(i + 1, 26):
-            candidates_index.append(f'{subindex}/{chr(i+ord("A"))}To{chr(j+ord("A"))}')
+            candidates_index.append(f'{subindex}/{chr(i + ord("A"))}To{chr(j + ord("A"))}')
     for i in range(10):
         for j in range(i + 1, 10):
             candidates_index.append(f'{subindex}/{i}To{j}')
     return candidates_index
 
 
+def get_subindexes_from_paragraph(paragraph_list, k):
+    paragraph = paragraph_list[k]
+    if "!!Examples:" in paragraph:  # remove Examples
+        return []
+    paragraph_lines = paragraph.split('<br>')
+    return [sanitize_link(i) for i in paragraph_lines if
+            i.startswith('+ ') or i.startswith('++ ') or i.startswith('+++ ')]
+
+
 def get_subindexes_from_index(page_src):
     try:
-        page_src = page_src.split('----')[1]
+        paragraph_list = page_src.split('----')
     except IndexError:
         return None
-    page_lines = page_src.split('<br>')
+    subindexes_from_page = []
+    size = len(paragraph_list)
+    for i in range(0, size):
+        subindexes_from_page.extend(get_tropes_from_paragraph(paragraph_list, i))
 
-    return [sanitize_link(i) for i in page_lines if i.startswith('+ ')]
+    return subindexes_from_page
+
+
+def get_tropes_from_paragraph(paragraph_list, k):
+    paragraph = paragraph_list[k]
+    if "!!Examples:" in paragraph:  # remove Examples
+        return []
+    paragraph_lines = paragraph.split('<br>')
+    return [sanitize_link(i) for i in paragraph_lines if
+            i.startswith('* ') or i.startswith('** ') or i.startswith('*** ')]
 
 
 def get_tropes_from_page(page_src):
     try:
-        page_src = page_src.split('----')[1]
+        paragraph_list = page_src.split('----')
     except IndexError:
         return None
+    tropes_from_page = []
+    size = len(paragraph_list)
+    for i in range(0, size):
+        tropes_from_page.extend(get_tropes_from_paragraph(paragraph_list, i))
 
-    if "!!Examples:" in page_src:  # remove Examples
-        return []
-
-    page_lines = page_src.split('<br>')
-
-    return [sanitize_link(i) for i in page_lines if i.startswith('* ')]
+    return tropes_from_page
 
 
 def check_not_found(page_src):
@@ -97,11 +122,13 @@ def check_not_found(page_src):
             return True
         return False
 
+
 def check_redirect(page_src):
     if '<span style="font-family:Courier, \'Courier New\', monospace">[[redirect:' in page_src:
         return True, page_src.split('[')[-1].split(']')[0].split(':')[-1]
     else:
         return False, None
+
 
 def catch_exception(exception, current_tropes, subindex, logging):
     if exception in current_tropes:
